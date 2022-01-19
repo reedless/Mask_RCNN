daf711e7b9d4b0e3b697aad9745178d61f961c4d
Howard <howardyanghaowen@gmail.com>
2022-01-19 15:37:16 +0800

    1 gpu it is

--- watermark_detector/train_watermarks.py
+++ watermark_detector/train_watermarks.py
@@ -32,10 +32,10 @@ class WatermarkConfig(Config):
     NAME = "watermark"
 
     # We use a GPU with 32GB memory
-    IMAGES_PER_GPU = 10
+    IMAGES_PER_GPU = 5
 
     # Uncomment to train on 4 GPUs (default is 1)
-    GPU_COUNT = 2
+    GPU_COUNT = 1
 
     # Number of classes (including background)
     NUM_CLASSES = 1 + 2  # watermark and text
